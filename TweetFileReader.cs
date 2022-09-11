d55722cf69f25d3d6fdaeb2f1626523630253e71
Themba Khumalo <TeeziNator@users.noreply.github.com>
2022-09-11 18:31:36 +0200

    File reader classes for different text files that implement the interface to read and process the file data.

---
+++ TweetFileReader.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TwitterFeedwithCore
+{
+    class TweetFileReader : IFileReader
+    {
+        //Implements the textFile property on the IFileReader interface
+        private string _tweetTextFile;
+        public string textFile
+        {
+            get => _tweetTextFile;
+            set => _tweetTextFile = value;
+        }
+
+        //Implements the dictionaryList property on the IFileReader interface
+        private IDictionary<string, List<string>> _userTweets;
+        public IDictionary<string, List<string>> dictionaryList
+        {
+            get => _userTweets;
+            set => _userTweets = value;
+        }
+
+        //Implements the users property on the IFileReader interface
+        private List<string> _users;
+        public List<string> users
+        {
+            get => _users;
+            set => _users = value;
+        }
+
+        //Variable to get and initialize the user followers collection
+        private readonly IDictionary<string, List<string>> userFollowers;
+
+        //Constructor to initialize values
+        public TweetFileReader(IDictionary<string, List<string>> d, List<string> u, string tweetTextFile, IDictionary<string, List<string>> f)
+        {
+            this.dictionaryList = d;
+            this.users = u;
+            this.textFile = tweetTextFile;
+            this.userFollowers = f;
+        }
+
+        //Implements the method to read and process the file data on the IFileReader interface
+        public void fileReader()
+        {
+            try
+            {
+                //Reading from the file using stream reader which does it line by line
+                StreamReader tweetFile = new StreamReader(_tweetTextFile); 
+
+                if (File.Exists(_tweetTextFile))
+                {
+                    using (tweetFile)
+                    {
+                        string line;
+                        string tweet;
+                        string tweetUser;
+
+                        while ((line = tweetFile.ReadLine()) != null)
+                        {
+                            string separatingStrings = ">";
+                            string[] tweetsList = line.Split(separatingStrings, StringSplitOptions.RemoveEmptyEntries);
+                            tweetUser = tweetsList[0]; //Name of user the tweets belong to (before the split)
+                            tweet = tweetsList[1]; //The tweets that belong to their users (after the split)
+
+                            //Adding the tweet to the user's tweets
+                            if (_userTweets.ContainsKey(tweetUser))
+                            {
+                                //Adds the tweets to the existing tweets if user has already tweeted
+                                List<string> existingTweets = _userTweets[tweetUser];
+                                existingTweets.Add($"\t @{tweetUser} : {tweet} ");
+                                _userTweets[tweetUser] = existingTweets;
+                            }
+                            else
+                            {
+                                //Adds a new tweet to the list if user has not tweeted yet
+                                List<string> uTweets = new List<string>
+                                {
+                                    $"\t @{tweetUser} : {tweet} "
+                                };
+                                _userTweets[tweetUser] = uTweets;
+                            }
+
+                            //List<string> keyList = new List<string>(userFollowers.Keys);
+                            //keyList = userFollowers[tweetUser];
+
+                            //Adding the tweet to the user's followers list
+                            List<string> uFollowers = userFollowers[tweetUser];
+
+                            if (uFollowers != null)
+                            {
+                                //Looping through the followers and adding the user's tweets to those followers list
+                                foreach (string f in uFollowers)
+                                {
+                                    if (_userTweets.ContainsKey(f))
+                                    {
+                                        //If the followers already has user's tweets, it add them to the existing list
+                                        List<string> followerTweets = _userTweets[f];
+                                        followerTweets.Add($"\t @{tweetUser} : {tweet} ");
+                                        _userTweets[f] = followerTweets;
+                                    }
+                                    else
+                                    {
+                                        //If not then add the new tweets
+                                        List<string> ft = new List<string>
+                                        {
+                                            $"\t @{tweetUser} : {tweet} "
+                                        };
+                                        _userTweets[f] = ft;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                tweetFile.Close(); //Dispose of the file once done using it
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());  //Catch file exception with detailed stacktrace
+            }
+        }
+    }
+}
---
+++ UserFileReader.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TwitterFeedwithCore
+{
+    class UserFileReader : IFileReader
+    {
+        //Implements the textFile property on the IFileReader interface
+        private string _userTextFile;
+        public string textFile
+        {
+            get => _userTextFile;
+            set => _userTextFile = value;
+        }
+
+        //Implements the dictionaryList property on the IFileReader interface
+        private IDictionary<string, List<string>> _userFollowers;
+        public IDictionary<string, List<string>> dictionaryList
+        {
+            get => _userFollowers;
+            set => _userFollowers = value;
+        }
+
+        //Implements the users property on the IFileReader interface
+        private List<string> _users;
+        public List<string> users
+        {
+            get => _users;
+            set => _users = value;
+        }
+
+        //Constructor to initialize values
+        public UserFileReader(IDictionary<string, List<string>> f, List<string> u, string userTextFile)
+        {
+            this.dictionaryList = f;
+            this.users = u;
+            this.textFile = userTextFile;
+        }
+
+        //Implements the method to read and process the file data on the IFileReader interface
+        public void fileReader()
+        {
+            try
+            {
+                //Reading from the file using stream reader which does it line by line
+                StreamReader userFile = new StreamReader(_userTextFile);
+
+                if (File.Exists(_userTextFile))
+                {
+                    using (userFile)
+                    {
+                        string line;
+                        string follower;
+                        string key;
+
+                        while ((line = userFile.ReadLine()) != null)
+                        {
+                            string[] separatingStrings = { " follows ", ", " };
+                            string[] strArray = line.Split(separatingStrings, StringSplitOptions.RemoveEmptyEntries);
+                            follower = strArray[0]; //Name of the user (before the split)
+
+                            //Looping through the user list and getting all the followers (after the split)
+                            if (!_users.Contains(follower))
+                            {
+                                _users.Add(follower);
+                            }
+
+                            for (int i = 0; i < strArray.Length; i++)
+                            {
+                                key = strArray[i];
+
+                                //Add the user to the dictionary if they already exist in the list
+                                if (_userFollowers.ContainsKey(key))
+                                {
+                                    List<string> existingList = _userFollowers[key];
+                                    _userFollowers[key] = existingList;
+                                }
+                                else
+                                {
+                                    //Adding the user followers to a new list if they do not exist
+                                    List<string> followersList = new List<string>
+                                    {
+                                        follower
+                                    };
+                                    _userFollowers[key] = followersList;
+                                }
+
+                                //Add all users to the dictionary collection
+                                if (!_users.Contains(key))
+                                {
+                                    _users.Add(key);
+                                }
+                            }
+                        }
+                    }
+                }
+                userFile.Close();  //Dispose of the file once done using it
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());  //Catch file exception with detailed stacktrace
+            }
+        }
+    }
+}
