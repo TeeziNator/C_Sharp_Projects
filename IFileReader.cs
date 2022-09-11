67160ee21a85f59b9a4d4099fffac907e7eef2cb
Themba Khumalo <TeeziNator@users.noreply.github.com>
2022-09-11 18:30:25 +0200

    Interface for reading contents of the file and adding them to a collection

---
+++ IFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwitterFeedwithCore
+{
+    interface IFileReader
+    {
+        string textFile { get; set; }  //Location of the file
+        IDictionary<string, List<string>> dictionaryList { get; set; }  //Collection of the data read from the file using a dictionary
+        List<string> users { get; set; } //List of users
+
+        void fileReader();  //Method to process the data read from the files
+    }
+}
